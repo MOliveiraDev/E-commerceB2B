3759645fd78175a14826ad7c41d129faa0118ac5
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-04-02 09:50:22 -0300

    first commit

---
+++ src/main/java/com/springboot/E_commerceB2B/ECommerceB2BApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.E_commerceB2B;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceB2BApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceB2BApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/E_commerceB2B/ECommerceB2BApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.E_commerceB2B;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceB2BApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
