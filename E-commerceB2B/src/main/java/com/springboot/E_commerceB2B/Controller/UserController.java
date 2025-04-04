1d4e7072bc483c58ff7f8059bd111def9a9f4aed
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-04-04 00:12:40 -0300

    Criando controle de usuário

---
+++ E-commerceB2B/src/main/java/com/springboot/E_commerceB2B/Controller/UserController.java
@@ -0,0 +1,28 @@
+package com.springboot.E_commerceB2B.Controller;
+
+import com.springboot.E_commerceB2B.Entities.User;
+import com.springboot.E_commerceB2B.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    // End points para criar usuário
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        User createdUser = userService.registerUser(user);
+        return ResponseEntity.ok(createdUser);
+    }
+
+
+}
