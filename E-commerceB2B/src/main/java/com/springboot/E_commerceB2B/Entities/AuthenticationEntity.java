92dc47a006ffaaa22108b1a79a549ac6c1af2298
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-04-04 00:13:01 -0300

    Update AuthenticationEntity.java

--- E-commerceB2B/src/main/java/com/springboot/E_commerceB2B/Entities/AuthenticationEntity.java
+++ E-commerceB2B/src/main/java/com/springboot/E_commerceB2B/Entities/AuthenticationEntity.java
@@ -8,14 +8,16 @@ import lombok.Setter;
 public class AuthenticationEntity {
 
     private String username;
+    private String email;
     private String password;
 
     public AuthenticationEntity() {
 
     }
 
-    public AuthenticationEntity(String username, String password) {
+    public AuthenticationEntity(String username, String password, String email) {
         this.username = username;
+        this.email = email;
         this.password = password;
     }
 
