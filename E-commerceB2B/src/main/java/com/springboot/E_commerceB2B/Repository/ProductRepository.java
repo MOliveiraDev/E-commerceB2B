686f24d29630f8046e45dcd09447d1f28be887d9
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-04-05 00:53:26 -0300

    Create ProductRepository

--- E-commerceB2B/src/main/java/com/springboot/E_commerceB2B/Repository/ProductRepository.java
+++ E-commerceB2B/src/main/java/com/springboot/E_commerceB2B/Repository/ProductRepository.java
@@ -1,4 +1,8 @@
 package com.springboot.E_commerceB2B.Repository;
 
-public interface ProductRepository {
+import com.springboot.E_commerceB2B.Entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
 }
