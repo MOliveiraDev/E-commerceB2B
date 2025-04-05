45a8f0998b3fbebe8ba29cbff1a06eb9284eedac
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-04-05 00:53:04 -0300

    Create OrderRepository

--- E-commerceB2B/src/main/java/com/springboot/E_commerceB2B/Repository/OrderRepository.java
+++ E-commerceB2B/src/main/java/com/springboot/E_commerceB2B/Repository/OrderRepository.java
@@ -1,4 +1,7 @@
 package com.springboot.E_commerceB2B.Repository;
 
-public interface OrderRepository {
+import com.springboot.E_commerceB2B.Entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository  extends JpaRepository <Order, Long> {
 }
